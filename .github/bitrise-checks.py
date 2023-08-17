6a61888c7824cc1f69de4667338607df3990ce31
Cagdas Caglak <cagdasc@users.noreply.github.com>
2023-08-17 15:19:45 +0100

    Update bitrise-checks.py

--- .github/bitrise-checks.py
+++ .github/bitrise-checks.py
@@ -1,6 +1,11 @@
 import os
 import json
 
+
+def print_json(json_data):
+    json_formatted_str = json.dumps(json_data, indent=2)
+    print(json_formatted_str)
+    
 event_data_path = os.environ.get('GITHUB_EVENT_PATH')
 
 if event_data_path:
@@ -8,5 +13,7 @@ if event_data_path:
         event_data = json.load(event_file)
         check_run_conclusion = event_data['check_run']['conclusion']
         print(f"Check run conclusion: {check_run_conclusion}")
+
+        print_json(event_data)
 else:
     print("Check run event data not provided.")
