dda0b94614fec58abeacb4b92f8905a7931114dd
Cagdas Caglak <cagdasc@users.noreply.github.com>
2023-08-17 15:11:54 +0100

    Update bitrise-checks.py

--- .github/bitrise-checks.py
+++ .github/bitrise-checks.py
@@ -1,20 +1,12 @@
 import os
 import json
 
-PAYLOAD = os.getenv("PAYLOAD", "Default paylod")
-PRS = os.getenv("PRS", None)
+event_data_path = os.environ.get('CHECK_RUN_CONCLUSION')
 
-def print_json(json_data):
-    json_formatted_str = json.dumps(json_data, indent=2)
-    print(json_formatted_str)
-
-print("Hello bitrise")
-print(PAYLOAD)
-print("------")
-print(PRS)
-
-print("------")
-print("------")
-
-print_json(PAYLOAD)
-print_json(PRS)
+if event_data_path:
+    with open(event_data_path, 'r') as event_file:
+        event_data = json.load(event_file)
+        check_run_conclusion = event_data['check_run']['conclusion']
+        print(f"Check run conclusion: {check_run_conclusion}")
+else:
+    print("Check run event data not provided.")
